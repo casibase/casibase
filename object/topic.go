ec1de8a4c5d513c0fc018efd2df38fed98276e43
Kininaru <shiftregister233@outlook.com>
2021-06-15 23:28:18 +0800

    fix: avatar display error in search result
    Signed-off-by: Kininaru <shiftregister233@outlook.com>

--- object/topic.go
+++ object/topic.go
@@ -662,13 +662,22 @@ func (t Topic) GetAllRepliesOfTopic() []string {
 	return ret
 }
 
-func SearchTopics(keyword string) []Topic {
-	var ret []Topic
+func SearchTopics(keyword string) []TopicWithAvatar {
+	var topics []Topic
 	keyword = fmt.Sprintf("%%%s%%", keyword)
 
-	err := adapter.Engine.Where("deleted = 0").Where("title like ? or content like ?", keyword, keyword).Find(&ret)
+	err := adapter.Engine.Where("deleted = 0").Where("title like ? or content like ?", keyword, keyword).Find(&topics)
 	if err != nil {
 		panic(err)
+	}
+
+	memberAvatar := GetMemberAvatarMapping()
+	var ret []TopicWithAvatar
+	for _, topic := range topics {
+		ret = append(ret, TopicWithAvatar{
+			Topic: topic,
+			Avatar: memberAvatar[topic.Author],
+		})
 	}
 
 	return ret
