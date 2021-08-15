78cf939ce3b6072829ee3cd6eb85fd8fed83fc4d
Yang Luo <hsluoyz@qq.com>
2021-08-15 22:10:24 +0800

    Fix edit box md mode.

--- web/src/main/EditBox.js
+++ web/src/main/EditBox.js
@@ -19,6 +19,7 @@ import * as TopicBackend from "../backend/TopicBackend";
 import * as ReplyBackend from "../backend/ReplyBackend";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import TagsInput from "react-tagsinput";
+import "codemirror/lib/codemirror.css";
 import "../tagsInput.css";
 import * as Tools from "./Tools";
 import i18next from "i18next";
