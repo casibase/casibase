a251c9fcdc122d38445e31bd99abe2b63603bcea
Gucheng Wang <nomeguy@qq.com>
2021-12-11 12:35:51 +0800

    Improve profile IdP icons.

--- web/src/main/MemberBox.js
+++ web/src/main/MemberBox.js
@@ -61,11 +61,7 @@ class MemberBox extends React.Component {
   }
 
   getFavoriteNum() {
-    if (
-      this.props.account === undefined ||
-      this.props.account === null ||
-      this.state.getFavoriteStatus
-    ) {
+    if (this.props.account === undefined || this.props.account === null || this.state.getFavoriteStatus) {
       return;
     }
 
@@ -134,14 +130,10 @@ class MemberBox extends React.Component {
       return (
         <div className="box">
           <div className="header">
-            <Link to="/">{Setting.getForumName()}</Link>{" "}
-            <span className="chevron">&nbsp;›&nbsp;</span>{" "}
-            {i18next.t("loading:Member profile is loading")}
+            <Link to="/">{Setting.getForumName()}</Link> <span className="chevron">&nbsp;›&nbsp;</span> {i18next.t("loading:Member profile is loading")}
           </div>
           <div className="cell">
-            <span className="gray bigger">
-              {i18next.t("loading:Please wait patiently...")}
-            </span>
+            <span className="gray bigger">{i18next.t("loading:Please wait patiently...")}</span>
           </div>
         </div>
       );
@@ -151,9 +143,7 @@ class MemberBox extends React.Component {
       return (
         <div className="box">
           <div className="header">
-            <Link to="/">{Setting.getForumName()}</Link>{" "}
-            <span className="chevron">&nbsp;›&nbsp;</span>{" "}
-            {i18next.t("error:Member does not exist")}
+            <Link to="/">{Setting.getForumName()}</Link> <span className="chevron">&nbsp;›&nbsp;</span> {i18next.t("error:Member does not exist")}
           </div>
           <div className="cell">
             <span className="gray bigger">404 Member Not Found</span>
@@ -165,90 +155,41 @@ class MemberBox extends React.Component {
       );
     }
 
-    const showWatch =
-      this.props.account !== undefined &&
-      this.props.account !== null &&
-      this.state.memberId !== this.props.account?.name;
-    const { goldCount, silverCount, bronzeCount } = scoreConverter(
-      this.state.member.score
-    );
+    const showWatch = this.props.account !== undefined && this.props.account !== null && this.state.memberId !== this.props.account?.name;
+    const { goldCount, silverCount, bronzeCount } = scoreConverter(this.state.member.score);
     return (
       <div className="box">
         <div className="cell">
           <table cellPadding="0" cellSpacing="0" border="0" width="100%">
             <tbody>
               <tr>
-                <td
-                  width={Setting.PcBrowser ? "73" : "56"}
-                  valign="top"
-                  align="center"
-                >
-                  <Avatar
-                    username={this.state.member?.name}
-                    size={Setting.PcBrowser ? "large" : "middle"}
-                    avatar={this.state.member?.avatar}
-                  />
+                <td width={Setting.PcBrowser ? "73" : "56"} valign="top" align="center">
+                  <Avatar username={this.state.member?.name} size={Setting.PcBrowser ? "large" : "middle"} avatar={this.state.member?.avatar} />
                   <div className="sep10" />
-                  {this.state.member?.onlineStatus ? (
-                    <strong className="online">
-                      {" "}
-                      {i18next.t("member:ONLINE")}{" "}
-                    </strong>
-                  ) : (
-                    ""
-                  )}
+                  {this.state.member?.onlineStatus ? <strong className="online"> {i18next.t("member:ONLINE")} </strong> : ""}
                 </td>
                 <td width="10" />
                 <td width="auto" valign="top" align="left">
-                  <div
-                    className="fr"
-                    style={{ display: showWatch ? "" : "none" }}
-                  >
+                  <div className="fr" style={{ display: showWatch ? "" : "none" }}>
                     {this.state.favoritesStatus ? (
-                      <input
-                        type="button"
-                        value={i18next.t("member:Cancel Following")}
-                        onClick={() =>
-                          this.deleteFavorite(this.state.member?.name)
-                        }
-                        className="super inverse button"
-                      />
+                      <input type="button" value={i18next.t("member:Cancel Following")} onClick={() => this.deleteFavorite(this.state.member?.name)} className="super inverse button" />
                     ) : (
-                      <input
-                        type="button"
-                        value={i18next.t("member:Watch")}
-                        onClick={() =>
-                          this.addFavorite(this.state.member?.name)
-                        }
-                        className="super special button"
-                      />
+                      <input type="button" value={i18next.t("member:Watch")} onClick={() => this.addFavorite(this.state.member?.name)} className="super special button" />
                     )}
                     <div className="sep10" />
-                    <input
-                      type="button"
-                      value="Block"
-                      onClick={this.block(this.state.memberId)}
-                      className="super normal button"
-                    />
+                    <input type="button" value="Block" onClick={this.block(this.state.memberId)} className="super normal button" />
                   </div>
-                  <h1 style={{ marginBottom: "5px" }}>
-                    {this.state.member?.name}
-                  </h1>
+                  <h1 style={{ marginBottom: "5px" }}>{this.state.member?.name}</h1>
                   <span className="bigger">{this.state.member?.tagline}</span>
                   <div className="sep10" />
-                  {this.state.member?.affiliation?.length !== 0 ||
-                  this.state.member?.title?.length !== 0 ? (
+                  {this.state.member?.affiliation?.length !== 0 || this.state.member?.title?.length !== 0 ? (
                     <span>
-                      🏢&nbsp; <strong>{this.state.member?.affiliation}</strong>{" "}
-                      / {this.state.member?.title}
+                      🏢&nbsp; <strong>{this.state.member?.affiliation}</strong> / {this.state.member?.title}
                     </span>
                   ) : null}
                   <div className={Setting.PcBrowser ? "sep10" : "sep5"} />
                   <span className="gray">
-                    {Setting.getForumName()} {i18next.t("member:No.")}{" "}
-                    {this.state.member?.ranking}{" "}
-                    {i18next.t("member:member, joined on")}{" "}
-                    {Setting.getFormattedDate(this.state.member?.createdTime)}
+                    {Setting.getForumName()} {i18next.t("member:No.")} {this.state.member?.ranking} {i18next.t("member:member, joined on")} {Setting.getFormattedDate(this.state.member?.createdTime)}
                     {/*{Setting.PcBrowser ? (*/}
                     {/*  <span>*/}
                     {/*    <div className="sep5" />*/}
@@ -257,40 +198,12 @@ class MemberBox extends React.Component {
                     {/*  </span>*/}
                     {/*) : null}*/}
                     <div className="sep5" />
-                    {this.state.member?.isModerator ? (
-                      <img
-                        src={Setting.getStatic("/img/mod@2x.png")}
-                        height="14px"
-                        align="absmiddle"
-                      />
-                    ) : null}{" "}
-                    {this.state.member?.isModerator
-                      ? i18next.t("member:authorized to manage the community")
-                      : null}
+                    {this.state.member?.isModerator ? <img src={Setting.getStatic("/img/mod@2x.png")} height="14px" align="absmiddle" /> : null} {this.state.member?.isModerator ? i18next.t("member:authorized to manage the community") : null}
                   </span>
                   <div className="sep10" />
                   <div className="balance_area">
-                    {goldCount}{" "}
-                    <img
-                      src={Setting.getStatic("/img/gold@2x.png")}
-                      height="16"
-                      alt="G"
-                      border="0"
-                    />{" "}
-                    {silverCount}{" "}
-                    <img
-                      src={Setting.getStatic("/img/silver@2x.png")}
-                      height="16"
-                      alt="S"
-                      border="0"
-                    />{" "}
-                    {bronzeCount}{" "}
-                    <img
-                      src={Setting.getStatic("/img/bronze@2x.png")}
-                      height="16"
-                      alt="B"
-                      border="0"
-                    />
+                    {goldCount} <img src={Setting.getStatic("/img/gold@2x.png")} height="16" alt="G" border="0" /> {silverCount} <img src={Setting.getStatic("/img/silver@2x.png")} height="16" alt="S" border="0" /> {bronzeCount}{" "}
+                    <img src={Setting.getStatic("/img/bronze@2x.png")} height="16" alt="B" border="0" />
                   </div>
                 </td>
               </tr>
@@ -300,35 +213,13 @@ class MemberBox extends React.Component {
         </div>
         <div className="widgets">
           {this.state.member?.homepage?.length !== 0 ? (
-            <a
-              href={this.state.member?.homepage}
-              className="social_label"
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-            >
-              <img
-                src={Setting.getStatic("/img/social_home.png")}
-                width="24"
-                alt="Website"
-                align="absmiddle"
-              />{" "}
-              &nbsp;{this.state.member?.homepage}
+            <a href={this.state.member?.homepage} className="social_label" target="_blank" rel="nofollow noopener noreferrer">
+              <img src={Setting.getStatic("/img/social_home.png")} width="24" alt="Website" align="absmiddle" /> &nbsp;{this.state.member?.homepage}
             </a>
           ) : null}
           {this.state.member?.location?.length !== 0 ? (
-            <a
-              href={`http://www.google.com/maps?q=${this.state.member?.location}`}
-              className="social_label"
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-            >
-              <img
-                src={Setting.getStatic("/img/social_geo.png")}
-                width="24"
-                alt="Geo"
-                align="absmiddle"
-              />{" "}
-              &nbsp;{this.state.member?.location}
+            <a href={`http://www.google.com/maps?q=${this.state.member?.location}`} className="social_label" target="_blank" rel="nofollow noopener noreferrer">
+              <img src={Setting.getStatic("/img/social_geo.png")} width="24" alt="Geo" align="absmiddle" /> &nbsp;{this.state.member?.location}
             </a>
           ) : null}
           {this.renderIdp(this.state.member, "GitHub")}
@@ -336,31 +227,25 @@ class MemberBox extends React.Component {
           {this.renderIdp(this.state.member, "WeChat")}
           {this.renderIdp(this.state.member, "QQ")}
         </div>
-        {this.state.member?.bio !== "" ? (
-          <div className="cell">{this.state.member?.bio}</div>
-        ) : null}
+        {this.state.member?.bio !== "" ? <div className="cell">{this.state.member?.bio}</div> : null}
       </div>
     );
-  }
-
-  getProviderLink(user, provider) {
-    if (provider.type === "GitHub") {
-      return `https://github.com/${this.getUserProperty(
-        user,
-        provider.type,
-        "username"
-      )}`;
-    } else if (provider.type === "Google") {
-      return "https://mail.google.com";
-    } else {
-      return "";
-    }
   }
 
   getUserProperty(user, providerType, propertyName) {
     const key = `oauth_${providerType}_${propertyName}`;
     if (user.properties === null) return "";
     return user.properties[key];
+  }
+
+  getProviderLink(user, provider, username) {
+    if (provider.type === "GitHub") {
+      return `https://github.com/${username}`;
+    } else if (provider.type === "Google") {
+      return `mailto:${username}`;
+    } else {
+      return "";
+    }
   }
 
   renderIdp(user, providerType) {
@@ -372,23 +257,18 @@ class MemberBox extends React.Component {
     const provider = { type: providerType };
 
     const linkedValue = user[provider.type.toLowerCase()];
-    const profileUrl = this.getProviderLink(user, provider);
     const id = this.getUserProperty(user, provider.type, "id");
     const username = this.getUserProperty(user, provider.type, "username");
-    const displayName = this.getUserProperty(
-      user,
-      provider.type,
-      "displayName"
-    );
+    const displayName = this.getUserProperty(user, provider.type, "displayName");
     const email = this.getUserProperty(user, provider.type, "email");
     let avatarUrl = this.getUserProperty(user, provider.type, "avatarUrl");
-
     if (avatarUrl === "" || avatarUrl === undefined) {
       avatarUrl = Setting.getProviderLogoLink(provider);
     }
 
-    let name =
-      username === undefined ? displayName : `${displayName} (${username})`;
+    const profileUrl = this.getProviderLink(user, provider, username);
+
+    let name = username === undefined ? displayName : `${displayName} (${username})`;
     if (name === undefined) {
       if (id !== undefined) {
         name = id;
@@ -400,14 +280,8 @@ class MemberBox extends React.Component {
     }
 
     return (
-      <a
-        href={profileUrl}
-        className="social_label"
-        target="_blank"
-        rel="nofollow noopener noreferrer"
-      >
-        <img src={avatarUrl} width="24" alt={providerType} align="absmiddle" />{" "}
-        &nbsp;{name}
+      <a href={profileUrl} style={profileUrl !== "" ? null : { pointerEvents: "none" }} className="social_label" target="_blank" rel="nofollow noopener noreferrer">
+        <img src={avatarUrl} width="24" style={{ borderRadius: "24px" }} alt={providerType} align="absmiddle" /> &nbsp;{name}
       </a>
     );
   }
@@ -422,43 +296,21 @@ class MemberBox extends React.Component {
           <tbody>
             <tr>
               <td width="33%" valign="center" align="center">
-                <Link
-                  to="/my/nodes"
-                  className="dark"
-                  style={{ display: "block" }}
-                >
+                <Link to="/my/nodes" className="dark" style={{ display: "block" }}>
                   <span className="bigger">{this.state.nodeFavoriteNum}</span>
                   <div className="sep3" />
                   <span className="fade small">{i18next.t("bar:Nodes")}</span>
                 </Link>
               </td>
-              <td
-                width="33%"
-                valign="center"
-                align="center"
-                style={{ borderLeft: "1px solid rgba(100, 100, 100, 0.25)" }}
-              >
-                <Link
-                  to="/my/topics"
-                  className="dark"
-                  style={{ display: "block" }}
-                >
+              <td width="33%" valign="center" align="center" style={{ borderLeft: "1px solid rgba(100, 100, 100, 0.25)" }}>
+                <Link to="/my/topics" className="dark" style={{ display: "block" }}>
                   <span className="bigger">{this.state.topicFavoriteNum}</span>
                   <div className="sep3" />
                   <span className="fade small">{i18next.t("bar:Topics")}</span>
                 </Link>
               </td>
-              <td
-                width="33%"
-                valign="center"
-                align="center"
-                style={{ borderLeft: "1px solid rgba(100, 100, 100, 0.25)" }}
-              >
-                <Link
-                  to="/my/following"
-                  className="dark"
-                  style={{ display: "block" }}
-                >
+              <td width="33%" valign="center" align="center" style={{ borderLeft: "1px solid rgba(100, 100, 100, 0.25)" }}>
+                <Link to="/my/following" className="dark" style={{ display: "block" }}>
                   <span className="bigger">{this.state.followingNum}</span>
                   <div className="sep3" />
                   <span className="fade small">{i18next.t("bar:Watch")}</span>
@@ -475,30 +327,13 @@ class MemberBox extends React.Component {
   render() {
     return (
       <span>
-        {Setting.PcBrowser ? (
-          <div className="sep20" />
-        ) : (
-          <div className="sep5" />
-        )}
+        {Setting.PcBrowser ? <div className="sep20" /> : <div className="sep5" />}
         {this.renderMember()}
-        {!Setting.PcBrowser &&
-        this.props.account?.name === this.state.memberId ? (
-          <div className="sep5" />
-        ) : null}
-        {!Setting.PcBrowser && this.props.account?.name === this.state.memberId
-          ? this.renderMemberFavorites()
-          : null}
-        {Setting.PcBrowser ? (
-          <div className="sep20" />
-        ) : (
-          <div className="sep5" />
-        )}
+        {!Setting.PcBrowser && this.props.account?.name === this.state.memberId ? <div className="sep5" /> : null}
+        {!Setting.PcBrowser && this.props.account?.name === this.state.memberId ? this.renderMemberFavorites() : null}
+        {Setting.PcBrowser ? <div className="sep20" /> : <div className="sep5" />}
         <AllCreatedTopicsBox member={this.state.member} />
-        {Setting.PcBrowser ? (
-          <div className="sep20" />
-        ) : (
-          <div className="sep5" />
-        )}
+        {Setting.PcBrowser ? <div className="sep20" /> : <div className="sep5" />}
         <LatestReplyBox member={this.state.member} />
       </span>
     );
