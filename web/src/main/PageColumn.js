c3a3eb0f60664b9a27cacfc60c3c715143966db2
Nekotoxin <nekotoxin@outlook.com>
2022-02-21 21:59:06 +0800

    modified ./web/src/main/PageColumn.js

--- web/src/main/PageColumn.js
+++ web/src/main/PageColumn.js
@@ -158,7 +158,8 @@ class PageColumn extends React.Component {
       this.props.onChange(page);
       return;
     }
-    // this.props.history.push(`${url}?p=${page}`);
+    this.props.history.push(`${url}?p=${page}`);
+    this.props.history.go(0);//fix :allow click
     Setting.goToLink(`${url}?p=${page}`);
   }
 
