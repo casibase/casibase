12560f9e9aed869d5503d3427d749068ae583bb9
Yang Luo <hsluoyz@qq.com>
2022-07-17 23:07:31 +0800

    Add ListFiles.

--- util/path.go
+++ util/path.go
@@ -1,10 +1,30 @@
 package util
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+)
 
 func FileExist(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false
 	}
 	return true
+}
+
+func ListFiles(path string) []string {
+	res := []string{}
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() {
+			res = append(res, f.Name())
+		}
+	}
+
+	return res
 }
