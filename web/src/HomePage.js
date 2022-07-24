0030a41fa6a1d5a99ae5b0610bc6bb3758d3dd83
Yang Luo <hsluoyz@qq.com>
2022-07-24 16:32:20 +0800

    Add file viewer to home page

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -1,16 +1,43 @@
 import React from "react";
+import {Spin} from "antd";
+import i18next from "i18next";
+import FileTree from "./FileTree";
+import * as StoreBackend from "./backend/StoreBackend";
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       classes: props,
-      wordset: null,
+      store: null,
     };
   }
 
+  componentWillMount() {
+    this.getStore();
+  }
+
+  getStore() {
+    StoreBackend.getStore(this.props.account.name, "openbrain")
+      .then((store) => {
+        this.setState({
+          store: store,
+        });
+      });
+  }
+
   render() {
-    return "Home";
+    if (this.state.store === null) {
+      return (
+        <div className="App">
+          <Spin size="large" tip={i18next.t("general:Loading...")} style={{paddingTop: "10%"}} />
+        </div>
+      );
+    }
+
+    return (
+      <FileTree domain={this.state.store.domain} tree={this.state.store.fileTree} />
+    );
   }
 }
 
