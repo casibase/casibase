f2fc307bdac41f2a02a1c6eb18ec3a4bc2e5966d
Yang Luo <hsluoyz@qq.com>
2022-08-29 21:34:12 +0800

    Improve theme color

--- web/craco.config.js
+++ web/craco.config.js
@@ -7,7 +7,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': 'rgb(92,48,125)', '@primary-1': 'rgba(92,48,125,0.55)' },
+            modifyVars: { '@primary-color': 'rgb(102,8,116)' },
             javascriptEnabled: true,
           },
         },
