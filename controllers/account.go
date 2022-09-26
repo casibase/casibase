18cb5e29cce95df9235c6fd9881ce6a5bf40a375
Yang Luo <hsluoyz@qq.com>
2022-09-26 09:43:39 +0800

    Improve error in Signin()

--- controllers/account.go
+++ controllers/account.go
@@ -30,12 +30,14 @@ func (c *ApiController) Signin() {
 
 	token, err := casdoorsdk.GetOAuthToken(code, state)
 	if err != nil {
-		panic(err)
+		c.ResponseError(err.Error())
+		return
 	}
 
 	claims, err := casdoorsdk.ParseJwtToken(token.AccessToken)
 	if err != nil {
-		panic(err)
+		c.ResponseError(err.Error())
+		return
 	}
 
 	claims.AccessToken = token.AccessToken
