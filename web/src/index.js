8379aeef4688a8df82f3c91952907c37cdbe9e0a
Yang Luo <hsluoyz@qq.com>
2023-07-07 23:29:48 +0800

    Update react to 18.2.0

--- web/src/index.js
+++ web/src/index.js
@@ -6,7 +6,7 @@ import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import "./index.css";
 import "./font.css";
 import App from "./App";
@@ -15,12 +15,15 @@ import * as serviceWorker from "./serviceWorker";
 import {BrowserRouter} from "react-router-dom";
 import "./i18n";
 
-ReactDOM.render((
+const container = document.getElementById("root");
+
+const app = createRoot(container);
+
+app.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-),
-document.getElementById("root"));
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
