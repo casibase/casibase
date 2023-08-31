43000075c07bf217e2ac6539cd5f0c2eebbd8afa
yehong <58927640+yehong-z@users.noreply.github.com>
2023-08-31 23:53:25 +0800

    feat: demo mode (#616)

--- controllers/util.go
+++ controllers/util.go
@@ -14,6 +14,8 @@
 
 package controllers
 
+import "github.com/astaxie/beego/context"
+
 type Response struct {
 	Status string      `json:"status"`
 	Msg    string      `json:"msg"`
@@ -64,4 +66,24 @@ func (c *ApiController) RequireAdmin() bool {
 	}
 
 	return false
+}
+
+func DenyRequest(ctx *context.Context) {
+	responseError(ctx, "Unauthorized operation")
+}
+
+func responseError(ctx *context.Context, error string, data ...interface{}) {
+	resp := Response{Status: "error", Msg: error}
+	switch len(data) {
+	case 2:
+		resp.Data2 = data[1]
+		fallthrough
+	case 1:
+		resp.Data = data[0]
+	}
+
+	err := ctx.Output.JSON(resp, true, false)
+	if err != nil {
+		panic(err)
+	}
 }
