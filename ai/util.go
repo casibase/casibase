4eeee5b3398faa71e180ce117748454eb00a8750
kv-chiu <kelvin.chiu021@outlook.com>
2023-09-09 18:40:36 +0800

    fix: modify retrieve functions

--- ai/util.go
+++ ai/util.go
@@ -15,8 +15,6 @@
 package ai
 
 import (
-	"math"
-
 	"github.com/pkoukk/tiktoken-go"
 )
 
@@ -29,33 +27,4 @@ func GetTokenSize(model string, prompt string) (int, error) {
 	token := tkm.Encode(prompt, nil, nil)
 	res := len(token)
 	return res, nil
-}
-
-func cosineSimilarity(vec1, vec2 []float32, vec1Norm float32) float32 {
-	dotProduct := dot(vec1, vec2)
-	vec2Norm := norm(vec2)
-	if vec2Norm == 0 {
-		return 0.0
-	}
-	return dotProduct / (vec1Norm * vec2Norm)
-}
-
-func dot(vec1, vec2 []float32) float32 {
-	if len(vec1) != len(vec2) {
-		panic("Vector lengths do not match")
-	}
-
-	dotProduct := float32(0.0)
-	for i := range vec1 {
-		dotProduct += vec1[i] * vec2[i]
-	}
-	return dotProduct
-}
-
-func norm(vec []float32) float32 {
-	normSquared := float32(0.0)
-	for _, val := range vec {
-		normSquared += val * val
-	}
-	return float32(math.Sqrt(float64(normSquared)))
 }
---
+++ retrievers/util.go
@@ -0,0 +1,32 @@
+package retrievers
+
+import "math"
+
+func cosineSimilarity(vec1, vec2 []float32, vec1Norm float32) float32 {
+	dotProduct := dot(vec1, vec2)
+	vec2Norm := norm(vec2)
+	if vec2Norm == 0 {
+		return 0.0
+	}
+	return dotProduct / (vec1Norm * vec2Norm)
+}
+
+func dot(vec1, vec2 []float32) float32 {
+	if len(vec1) != len(vec2) {
+		panic("Vector lengths do not match")
+	}
+
+	dotProduct := float32(0.0)
+	for i := range vec1 {
+		dotProduct += vec1[i] * vec2[i]
+	}
+	return dotProduct
+}
+
+func norm(vec []float32) float32 {
+	normSquared := float32(0.0)
+	for _, val := range vec {
+		normSquared += val * val
+	}
+	return float32(math.Sqrt(float64(normSquared)))
+}
