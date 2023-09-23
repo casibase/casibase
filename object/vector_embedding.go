28f752c1a5e9934ac9fe5afcdfdf9492f5589c1a
yehong <58927640+yehong-z@users.noreply.github.com>
2023-09-23 21:10:38 +0800

    feat: support HNSW vector search algorithm (#646)
    Signed-off-by: yehong <239859435@qq.com>

--- object/vector_embedding.go
+++ object/vector_embedding.go
@@ -155,16 +155,10 @@ func GetNearestVectorText(embeddingProvider embedding.EmbeddingProvider, owner s
 		return "", fmt.Errorf("no qVector found")
 	}
 
-	vectors, err := getRelatedVectors(owner)
+	searchProvider, err := GetSearchProvider("Default", owner)
 	if err != nil {
 		return "", err
 	}
 
-	var nVectors [][]float32
-	for _, candidate := range vectors {
-		nVectors = append(nVectors, candidate.Data)
-	}
-
-	i := getNearestVectorIndex(qVector, nVectors)
-	return vectors[i].Text, nil
+	return searchProvider.Search(qVector)
 }
