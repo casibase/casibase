2e41d96707019a32894aa862aa7e07ad3a9c9456
曾叶鸿 <1059463820@qq.com>
2023-09-29 19:43:01 +0800

    feat: image message

--- web/src/ChatBox.js
+++ web/src/ChatBox.js
@@ -13,20 +13,64 @@
 // limitations under the License.
 
 import React from "react";
-import {Avatar, ChatContainer, ConversationHeader, MainContainer, Message, MessageInput, MessageList} from "@chatscope/chat-ui-kit-react";
+import {
+  Avatar,
+  Button,
+  ChatContainer,
+  ConversationHeader,
+  InputToolbox,
+  MainContainer,
+  Message,
+  MessageInput,
+  MessageList, SendButton
+} from "@chatscope/chat-ui-kit-react";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {FolderOutlined} from "@ant-design/icons";
 
 const aiAvatar = "https://cdn.casbin.com/casdoor/static/gpt.png";
 
 class ChatBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      content: "",
+    };
   }
-
-  handleSend = (innerHtml, textContent) => {
-    this.props.sendMessage(textContent);
+  handleSend = () => {
+    if (this.state.content !== "") {
+      this.props.sendMessage(this.state.content);
+      this.setState({content: ""});
+    }
   };
+  handleImageClick = () => {
+    if (this.inputImage.files) {
+      this.inputImage.click();
+    }
+  };
+  handleInputChange = async() => {
+    const file = this.inputImage.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const img = new Image();
+      img.onload = () => {
+        const originalWidth = img.width;
+        const originalHeight = img.height;
+        const maxWidth = 800;
+        let Ratio = 1;
+        if (originalWidth > maxWidth) {
+          Ratio = maxWidth / originalWidth;
+        }
+        const scaledWidth = Math.round(originalWidth * Ratio);
+        const scaledHeight = Math.round(originalHeight * Ratio);
+        this.setState({
+          content: this.state.content + `<img src="${img.src}" alt="${img.alt}" width="${scaledWidth}" height="${scaledHeight}">`,
+        });
+      };
 
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  };
   render() {
     let messages = this.props.messages;
     if (messages === null) {
@@ -51,7 +95,19 @@ class ChatBox extends React.Component {
               </Message>
             ))}
           </MessageList>
-          <MessageInput placeholder="Type message here" onSend={this.handleSend} />
+          <MessageInput placeholder="Type message here" value={this.state.content} onChange={val => this.setState({content: val})} onSend={this.handleSend} sendButton={false} attachButton={false} />
+          <InputToolbox>
+            <Button onClick={this.handleImageClick}><FolderOutlined /></Button>
+            <input
+              ref={e => this.inputImage = e}
+              type="file"
+              accept="image/*"
+              multiple={false}
+              onChange={this.handleInputChange}
+              style={{display: "none"}}
+            />
+            <SendButton onClick={this.handleSend} />
+          </InputToolbox>
         </ChatContainer>
       </MainContainer>
     );
