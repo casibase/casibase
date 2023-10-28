8cb763ff995ec2e695777d4dd1d659c1089e5868
kv-chiu <kelvin.chiu021@outlook.com>
2023-10-28 16:47:21 +0800

    fix: modify parseOpenAiMessages function

--- model/openai_util.go
+++ model/openai_util.go
@@ -81,15 +81,15 @@ func getOpenAiModelType(model string) string {
 
 func parseOpenAiMessages(messages []*RawMessage) []openai.ChatCompletionMessage {
 	var res []openai.ChatCompletionMessage
-	for i, message := range messages {
-		if i%2 == 0 {
+	for _, message := range messages {
+		if message.Author == "AI" {
 			res = append(res, openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleUser,
+				Role:    openai.ChatMessageRoleAssistant,
 				Content: message.Text,
 			})
 		} else {
 			res = append(res, openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleAssistant,
+				Role:    openai.ChatMessageRoleUser,
 				Content: message.Text,
 			})
 		}
