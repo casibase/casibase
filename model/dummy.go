1dddde840075ce9437895dc3ff411bde1d02bba0
Marrrrrrrrrrtin <90629140+MartinRepo@users.noreply.github.com>
2024-05-01 18:07:55 +0100

    feat: support dummy model provider and dummy embedding provider (#798)
    * feat: support dummy model provider
    
    * feat: support dummy model provider
    
    * feat: support dummy model provider

---
+++ model/dummy.go
@@ -0,0 +1,48 @@
+// Copyright 2024 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"io"
+)
+
+type DummyModelProvider struct {
+	subType string
+}
+
+func NewDummyModelProvider(subType string) (*DummyModelProvider, error) {
+	return &DummyModelProvider{
+		subType: subType,
+	}, nil
+}
+
+func (c *DummyModelProvider) GetPricing() string {
+	return `URL:
+This is a dummy module provider.
+
+Generate Model:
+
+This is a dummy module provider.
+`
+}
+
+func (p *DummyModelProvider) QueryText(message string, writer io.Writer, chat_history []*RawMessage, prompt string, knowledgeMessages []*RawMessage) (*ModelResult, error) {
+	answer := "this is the answer for \"" + message + "\""
+	err := flushDataAzure(answer, writer)
+	if err != nil {
+		return nil, err
+	}
+	return &ModelResult{}, nil
+}
--- object/init.go
+++ object/init.go
@@ -45,8 +45,8 @@ func initBuiltInStore() {
 		StorageProvider:   "provider-storage-built-in",
 		ImageProvider:     "",
 		SplitProvider:     "Default",
-		ModelProvider:     "",
-		EmbeddingProvider: "",
+		ModelProvider:     "dummy-model-provider",
+		EmbeddingProvider: "dummy-embedding-provider",
 		MemoryLimit:       20,
 		Frequency:         10000,
 		LimitMinutes:      10,
@@ -61,12 +61,21 @@ func initBuiltInStore() {
 }
 
 func initBuiltInProvider() {
-	provider, err := GetDefaultStorageProvider()
+	storageProvider, err := GetDefaultStorageProvider()
 	if err != nil {
 		panic(err)
 	}
 
-	if provider != nil {
+	modelProvider, err := GetDefaultModelProvider()
+	if err != nil {
+		panic(err)
+	}
+
+	embeddingProvider, err := GetDefaultEmbeddingProvider()
+	if err != nil {
+		panic(err)
+	}
+	if storageProvider != nil || modelProvider != nil || embeddingProvider != nil {
 		return
 	}
 
@@ -75,7 +84,7 @@ func initBuiltInProvider() {
 		path = "C:/storage_casibase"
 	}
 
-	provider = &Provider{
+	storageProvider = &Provider{
 		Owner:       "admin",
 		Name:        "provider-storage-built-in",
 		CreatedTime: util.GetCurrentTime(),
@@ -84,7 +93,35 @@ func initBuiltInProvider() {
 		Type:        "Local File System",
 		ClientId:    path,
 	}
-	_, err = AddProvider(provider)
+	_, err = AddProvider(storageProvider)
+	if err != nil {
+		panic(err)
+	}
+
+	modelProvider = &Provider{
+		Owner:       "admin",
+		Name:        "dummy-model-provider",
+		CreatedTime: util.GetCurrentTime(),
+		DisplayName: "Dummy Model Provider",
+		Category:    "Model",
+		Type:        "Dummy",
+		SubType:     "Dummy",
+	}
+	_, err = AddProvider(modelProvider)
+	if err != nil {
+		panic(err)
+	}
+
+	embeddingProvider = &Provider{
+		Owner:       "admin",
+		Name:        "dummy-embedding-provider",
+		CreatedTime: util.GetCurrentTime(),
+		DisplayName: "Dummy Embedding Provider",
+		Category:    "Embedding",
+		Type:        "Dummy",
+		SubType:     "Dummy",
+	}
+	_, err = AddProvider(embeddingProvider)
 	if err != nil {
 		panic(err)
 	}
