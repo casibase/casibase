ccf758246282c47d9b5833a42d9a8a1ad31739d7
sherlock <116318567+Sherlocksuper@users.noreply.github.com>
2024-05-05 18:02:10 +0800

    fix

--- web/src/ChatBox.js
+++ web/src/ChatBox.js
@@ -156,10 +156,6 @@ class ChatBox extends React.Component {
                     this.setState({value: val});
                   }}
                   value={this.state.value}
-                 // onPaste={(evt) => {
-                   // evt.preventDefault();
-                   // this.setState({value: this.state.value + evt.clipboardData.getData("text")});
-                 // }}
                   onAttachClick={() => {
                     this.handleImageClick();
                   }}
