0deefe74fd0051640808762f015ef100ddb611c0
zjuldt <3190105453@zju.edu.cn>
2024-06-06 16:57:21 +0800

    feat: support for post-answer function

--- web/src/ChatBox.js
+++ web/src/ChatBox.js
@@ -154,11 +154,11 @@ class ChatBox extends React.Component {
     });
   }
 
-  likeMesage() {
+  likeMessage() {
     Setting.showMessage("success", "Message liked successfully");
   }
 
-  dislikeMesage() {
+  dislikeMessage() {
     Setting.showMessage("success", "Message disliked successfully");
   }
 
@@ -206,12 +206,12 @@ class ChatBox extends React.Component {
                           <>
                             <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="22" height="22"><path d="M885.9 533.7c16.8-22.2 26.1-49.4 26.1-77.7 0-44.9-25.1-87.4-65.5-111.1a67.67 67.67 0 0 0-34.3-9.3H572.4l6-122.9c1.4-29.7-9.1-57.9-29.5-79.4-20.5-21.5-48.1-33.4-77.9-33.4-52 0-98 35-111.8 85.1l-85.9 311h-0.3v428h472.3c9.2 0 18.2-1.8 26.5-5.4 47.6-20.3 78.3-66.8 78.3-118.4 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7 0-12.6-1.8-25-5.4-37 16.8-22.2 26.1-49.4 26.1-77.7-0.2-12.6-2-25.1-5.6-37.1zM112 528v364c0 17.7 14.3 32 32 32h65V496h-65c-17.7 0-32 14.3-32 32z" fill="#cdcdcd"></path></svg>
                           </>
-                        } style={{border: "none"}} onClick={() => this.likeMesage()}></Button>}
+                        } style={{border: "none"}} onClick={() => this.likeMessage()}></Button>}
                         {<Button icon={
                           <>
                             <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="22" height="22"><path d="M885.9 490.3c3.6-12 5.4-24.4 5.4-37 0-28.3-9.3-55.5-26.1-77.7 3.6-12 5.4-24.4 5.4-37 0-28.3-9.3-55.5-26.1-77.7 3.6-12 5.4-24.4 5.4-37 0-51.6-30.7-98.1-78.3-118.4-8.3-3.6-17.2-5.4-26.5-5.4H273v428h0.3l85.8 310.8C372.9 889 418.9 924 470.9 924c29.7 0 57.4-11.8 77.9-33.4 20.5-21.5 31-49.7 29.5-79.4l-6-122.9h239.9c12.1 0 23.9-3.2 34.3-9.3 40.4-23.5 65.5-66.1 65.5-111 0-28.3-9.3-55.5-26.1-77.7zM112 132v364c0 17.7 14.3 32 32 32h65V100h-65c-17.7 0-32 14.3-32 32z" fill="#cdcdcd"></path></svg>
                           </>
-                        } style={{border: "none"}} onClick={() => this.dislikeMesage()}></Button>}
+                        } style={{border: "none"}} onClick={() => this.dislikeMessage()}></Button>}
                       </Message.Footer>
                     ) : null
                   }
