dbca9b493f211eb9a7fa52613ffd13e921ecd891
Yang Luo <hsluoyz@gmail.com>
2025-02-16 16:29:57 +0800

    feat: add state to store

--- web/src/ProviderEditPage.js
+++ web/src/ProviderEditPage.js
@@ -552,7 +552,7 @@ class ProviderEditPage extends React.Component {
         }
         <Row style={{marginTop: "20px"}}>
           <Col style={{marginTop: "5px"}} span={(Setting.isMobile()) ? 22 : 2}>
-            {i18next.t("provider:State")} :
+            {i18next.t("general:State")} :
           </Col>
           <Col span={22}>
             <Select virtual={false} style={{width: "100%"}} value={this.state.provider.state} onChange={value => {
