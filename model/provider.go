28304ebc5560cbf4488e2146743aacfa5afe4244
IsAurora6 <85173010+IsAurora6@users.noreply.github.com>
2025-02-26 22:08:31 +0800

    feat: add the Ollama model provider (#1026)

--- model/provider.go
+++ model/provider.go
@@ -41,7 +41,9 @@ type ModelProvider interface {
 func GetModelProvider(typ string, subType string, clientId string, clientSecret string, temperature float32, topP float32, topK int, frequencyPenalty float32, presencePenalty float32, providerUrl string, apiVersion string, compitableProvider string) (ModelProvider, error) {
 	var p ModelProvider
 	var err error
-	if typ == "Local" {
+	if typ == "Ollama" {
+		p, err = NewLocalModelProvider("Local", "custom-model", clientSecret, temperature, topP, 0, 0, providerUrl, subType)
+	} else if typ == "Local" {
 		p, err = NewLocalModelProvider(typ, subType, clientSecret, temperature, topP, frequencyPenalty, presencePenalty, providerUrl, compitableProvider)
 	} else if typ == "OpenAI" {
 		p, err = NewOpenAiModelProvider(typ, subType, clientSecret, temperature, topP, frequencyPenalty, presencePenalty)
