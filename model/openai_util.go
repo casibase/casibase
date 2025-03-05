d328d330a49087719ea7961dabf37f88591e4438
随风 <2721885092@qq.com>
2025-03-05 14:50:55 +0800

    fix: fix subtype is gpt-4.5-preview cause can't correctly judge Openai_util.getOpenAiModelType() method

--- model/openai_util.go
+++ model/openai_util.go
@@ -66,7 +66,7 @@ func getOpenAiModelType(model string) string {
 		"gpt-4-1106-preview", "gpt-4-turbo-preview", "gpt-4-vision-preview",
 		"gpt-4-1106-vision-preview", "gpt-4", "gpt-4-0613",
 		"gpt-4-32k", "gpt-4-32k-0613", "gpt-4o", "gpt-4o-2024-05-13", "gpt-4o-mini",
-		"gpt-4o-mini-2024-07-18", "gpt-4.5-preview-2025-02-27", "custom-model",
+		"gpt-4o-mini-2024-07-18", "gpt-4.5-preview-2025-02-27", "custom-model", "gpt-4.5-preview",
 	}
 
 	completionModels := []string{
