2131f935951541bc593231a001d3d5423988f5b0
Yang Luo <hsluoyz@gmail.com>
2025-03-09 11:09:21 +0800

    Improve ip_maxmind.go code

--- util/ip_maxmind.go
+++ util/ip_maxmind.go
@@ -1,3 +1,17 @@
+// Copyright 2025 The Casibase Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package util
 
 import (
@@ -23,7 +37,7 @@ func InitMaxmindDb() error {
 	if cityErr != nil {
 		maxmindCityDB, cityErr = geoip2.Open("../data/GeoLite2-City.mmdb")
 		if cityErr != nil {
-			fmt.Println("Init MaxMind City database failed")
+			fmt.Printf("InitMaxmindDb() open \"GeoLite2-City.mmdb\" warning: %v\n", cityErr)
 		}
 	}
 
@@ -32,7 +46,7 @@ func InitMaxmindDb() error {
 	if asnErr != nil {
 		maxmindASNDB, asnErr = geoip2.Open("../data/GeoLite2-ASN.mmdb")
 		if asnErr != nil {
-			fmt.Println("Init MaxMind ASN database failed")
+			fmt.Printf("InitMaxmindDb() open \"GeoLite2-ASN.mmdb\" warning: %v\n", asnErr)
 		}
 	}
 
