0affd2276b97209f4d1b8175c9d7bc9d41effd4c
Working On It <15123120+working-on-it@user.noreply.gitee.com>
2025-04-04 20:34:38 +0800

    fix: modify preview mode

--- web/src/chat/MessageActions.js
+++ web/src/chat/MessageActions.js
@@ -84,7 +84,6 @@ const MessageActions = ({
         icon={<CopyOutlined />}
         style={{border: "none", color: ThemeDefault.colorPrimary}}
         onClick={() => onCopy(message.html.props.dangerouslySetInnerHTML.__html)}
-        preview-clickable="true"
       />
 
       {!isLastMessage ? null : (
@@ -124,7 +123,6 @@ const MessageActions = ({
           }}
           onClick={() => onToggleRead(message)}
           disabled={isCurrentMessageBeingLoaded}
-          preview-clickable="true"
         />
       </Tooltip>
     </Space>
