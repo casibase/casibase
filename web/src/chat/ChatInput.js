d9cd3184159f56bbf24e5b5562a8ec034597f769
Yang Luo <hsluoyz@gmail.com>
2025-04-21 16:35:43 +0800

    feat: enable speech-to-text button by default

--- web/src/chat/ChatInput.js
+++ web/src/chat/ChatInput.js
@@ -99,7 +99,8 @@ const ChatInput = ({
     );
   }
 
-  const isSpeechDisabled = store?.speechToTextProvider === "";
+  // const isSpeechDisabled = store?.speechToTextProvider === "";
+  const isSpeechDisabled = false;
 
   return (
     <div style={{position: "absolute", bottom: 0, left: 0, right: 0, padding: "16px 24px", zIndex: 1}}>
