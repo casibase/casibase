4d2eb77a8b4eed915121973bd503ad8f4091a689
XIAOZHUOWU <77263323+XIAOZHUOWU@users.noreply.github.com>
2025-04-24 01:30:41 +0800

    feat: support i18n in Setting.showMessage() (#1187)

--- web/src/NodeListPage.js
+++ web/src/NodeListPage.js
@@ -56,13 +56,13 @@ class NodeListPage extends BaseListPage {
       .then((res) => {
         if (res.status === "ok") {
           this.props.history.push({pathname: `/nodes/${newNode.name}`, mode: "add"});
-          Setting.showMessage("success", "Node added successfully");
+          Setting.showMessage("success", i18next.t("general:Successfully added"));
         } else {
-          Setting.showMessage("error", `Failed to add Node: ${res.msg}`);
+          Setting.showMessage("error", `${i18next.t("general:Failed to add")}: ${res.msg}`);
         }
       })
       .catch(error => {
-        Setting.showMessage("error", `Node failed to add: ${error}`);
+        Setting.showMessage("error", `${i18next.t("general:Failed to add")}: ${error}`);
       });
   }
 
@@ -70,7 +70,7 @@ class NodeListPage extends BaseListPage {
     NodeBackend.deleteNode(this.state.data[i])
       .then((res) => {
         if (res.status === "ok") {
-          Setting.showMessage("success", "Node deleted successfully");
+          Setting.showMessage("success", i18next.t("general:Successfully deleted"));
           this.setState({
             data: Setting.deleteRow(this.state.data, i),
             pagination: {
@@ -79,11 +79,11 @@ class NodeListPage extends BaseListPage {
             },
           });
         } else {
-          Setting.showMessage("error", `Failed to delete Node: ${res.msg}`);
+          Setting.showMessage("error", `${i18next.t("general:Failed to delete")}: ${res.msg}`);
         }
       })
       .catch(error => {
-        Setting.showMessage("error", `Node failed to delete: ${error}`);
+        Setting.showMessage("error", `${i18next.t("general:Failed to delete")}: ${error}`);
       });
   }
 
