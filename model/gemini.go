ec80dbf7b92217f679eb0d1a51396725678b35ae
Yang Luo <hsluoyz@gmail.com>
2025-06-11 23:42:59 +0800

    Update gemini.go

--- model/gemini.go
+++ model/gemini.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The Casibase Authors. All Rights Reserved.
+// Copyright 2023 The Casibase Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -139,13 +139,7 @@ func (p *GeminiModelProvider) QueryText(question string, writer io.Writer, histo
 	}
 
 	messages := GenaiRawMessagesToMessages(question, history)
-
-	resp, err := model.GenerateContent(
-		ctx,
-		p.subType,
-		messages,
-		nil,
-	)
+	resp, err := model.GenerateContent(ctx, p.subType, messages, nil)
 	if err != nil {
 		return nil, err
 	}
