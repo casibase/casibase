44b8e81c80f3b372fdbe9b965ef3d233e562e88b
mayunxiang <2721885092@qq.com>
2025-07-06 23:38:06 +0800

    feat: add the HierarchySearchProvider.

--- object/search_hierarchy.go
+++ object/search_hierarchy.go
@@ -15,7 +15,11 @@
 package object
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/casibase/casibase/embedding"
+	"github.com/casibase/casibase/model"
 )
 
 type HierarchySearchProvider struct {
@@ -27,5 +31,66 @@ func NewHierarchySearchProvider(owner string) (*HierarchySearchProvider, error) 
 }
 
 func (p *HierarchySearchProvider) Search(storeName string, embeddingProviderName string, embeddingProviderObj embedding.EmbeddingProvider, modelProviderName string, text string, knowledgeCount int) ([]Vector, *embedding.EmbeddingResult, error) {
-	return nil, nil, nil
+	vectors, err := getRelatedVectors(storeName, embeddingProviderName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var (
+		vectorData [][]float32
+		titleMap   = make(map[string]bool)
+	)
+	for _, candidate := range vectors {
+		if candidate.File != "" && strings.HasSuffix(candidate.File, ".md") {
+			parts := strings.SplitN(candidate.Text, "\n\n", 2)
+			if len(parts) > 0 {
+				titleMap[parts[0]] = true
+			}
+			vectorData = append(vectorData, candidate.Data)
+		}
+	}
+	titleCandidates := make([]string, 0, len(titleMap))
+	for title := range titleMap {
+		titleCandidates = append(titleCandidates, title)
+	}
+
+	question, _, err := getEnhancedQuestionByModel(modelProviderName, text, titleCandidates, knowledgeCount)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	qVector, embeddingResult, err := queryVectorSafe(embeddingProviderObj, question)
+	if err != nil {
+		return nil, nil, err
+	}
+	if qVector == nil || len(qVector) == 0 {
+		return nil, nil, fmt.Errorf("no qVector found")
+	}
+
+	similarities, err := getNearestVectors(qVector, vectorData, knowledgeCount)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	res := []Vector{}
+	for _, similarity := range similarities {
+		vector := vectors[similarity.Index]
+		vector.Score = similarity.Similarity
+		res = append(res, *vector)
+	}
+
+	return res, embeddingResult, nil
+}
+
+func getEnhancedQuestionByModel(modelProviderName string, text string, titleCandidates []string, candidateTitlesNum int) (string, *model.ModelResult, error) {
+	prompt := fmt.Sprintf("Please help me select the top %d titles that are most likely to contain the answer. Just return the title list. No other content.", candidateTitlesNum)
+
+	question := fmt.Sprintf("Please select the titles most relevant to the following question and choose the %v most relevant items. Just return the title list. No other content.\nquestion:\n %s \n\nTitles: \n%s", candidateTitlesNum, text, "• "+strings.Join(titleCandidates, "\n• "))
+
+	res, modelResult, err := GetAnswerWithPrompt(modelProviderName, question, prompt)
+	if err != nil {
+		return "", nil, err
+	}
+	enhancedQuestion := fmt.Sprintf("%s\n\nrelated contents: %s", text, res)
+	return enhancedQuestion, modelResult, nil
 }
