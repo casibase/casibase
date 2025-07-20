32a6f364b6937c7de5b9a4ff1dabce8757232827
mayunxiang <2721885092@qq.com>
2025-07-20 22:13:05 +0800

    feat: compatible SDK carrying authorization call

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func main() {
 
 	beego.SetStaticPath("/swagger", "swagger")
 	beego.InsertFilter("*", beego.BeforeRouter, routers.StaticFilter)
+	beego.InsertFilter("*", beego.BeforeRouter, routers.AutoSigninFilter)
 	beego.InsertFilter("*", beego.BeforeRouter, routers.AuthzFilter)
 	beego.InsertFilter("*", beego.BeforeRouter, routers.PrometheusFilter)
 	beego.InsertFilter("*", beego.BeforeRouter, routers.RecordMessage)
