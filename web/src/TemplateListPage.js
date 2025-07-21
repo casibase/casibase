777381cfe87939908264e361d5b0eec334e8b225
dark-Qy <76839952+dark-Qy@users.noreply.github.com>
2025-07-21 10:13:03 +0800

    feat: fix adding template bug (#1402)

--- web/src/TemplateListPage.js
+++ web/src/TemplateListPage.js
@@ -47,8 +47,14 @@ class TemplateListPage extends BaseListPage {
     TemplateBackend.addTemplate(newTemplate)
       .then((res) => {
         if (res.status === "ok") {
-          this.props.history.push({pathname: `/templates/${newTemplate.name}`, mode: "add"});
           Setting.showMessage("success", i18next.t("general:Successfully added"));
+          this.setState({
+            data: Setting.prependRow(this.state.data, newTemplate),
+            pagination: {
+              ...this.state.pagination,
+              total: this.state.pagination.total + 1,
+            },
+          });
         } else {
           Setting.showMessage("error", `${i18next.t("general:Failed to add")}: ${res.msg}`);
         }
