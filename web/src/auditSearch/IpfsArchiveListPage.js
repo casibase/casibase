afbfc429ca52a0e46b154c2019f425c60aab954b
jjq0425 <419875574@qq.com>
2025-09-01 15:32:35 +0800

    fix: 移除页面灰度测试提示信息

--- web/src/auditSearch/IpfsArchiveListPage.js
+++ web/src/auditSearch/IpfsArchiveListPage.js
@@ -404,9 +404,7 @@ class IpfsArchiveListPage extends BaseListPage {
 
     return (
       <div>
-        <div style={{ color: 'red', fontSize: '24px', fontWeight: 'bold', textAlign: 'center', marginBottom: '20px' }}>
-          页面灰度测试与编码中！！仅部分功能可用！
-        </div>
+
 
         {/* 队列信息卡片 */}
         <Card
