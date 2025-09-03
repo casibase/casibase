c1610f9d92a490097c0267eb1fd1fe6c1a08b55c
code-with-zen <1530442576@qq.com>
2025-09-02 23:03:54 -0700

    feat: Update the model names, pricing and code for alibabacloud

--- model/alibabacloud.go
+++ model/alibabacloud.go
@@ -39,43 +39,62 @@ func (p *AlibabacloudModelProvider) GetPricing() string {
 	return `URL:
 https://help.aliyun.com/zh/model-studio/billing-for-model-studio
 
-| Model               | sub-type                        | Input Price per 1K characters    | Output Price per 1K characters |
-|---------------------|---------------------------------|----------------------------------|--------------------------------|
-| Qwen-Long           | qwen-long                       | 0.0005yuan/1,000 tokens          | 0.002yuan/1,000 tokens         |
-| Qwen-Turbo          | qwen-turbo                      | 0.002yuan/1,000 tokens           | 0.006yuan/1,000 tokens         |
-| Qwen-Plus           | qwen-plus                       | 0.004yuan/1,000 tokens           | 0.012yuan/1,000 tokens         |
-| Qwen-Max            | qwen-max                        | 0.04yuan/1,000 tokens            | 0.12yuan/1,000 tokens          |
-| Qwen-Max            | qwen-max-longcontext            | 0.04yuan/1,000 tokens            | 0.12yuan/1,000 tokens          |
-| Qwen3-235B-a22B     | qwen3-235b-a22b                 | 0.004yuan/1,000 tokens            | 0.04yuan/1,000 tokens         |
-| Qwen3-32B           | qwen3-32b            			| 0.002yuan/1,000 tokens            | 0.02yuan/1,000 tokens         |
-| DeepSeek-R1         | deepseek-r1                     | 0.002yuan/1,000 tokens           | 0.008yuan/1,000 tokens         |
-| DeepSeek-V3         | deepseek-v3                     | 0.001yuan/1,000 tokens           | 0.004yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-1.5b   | 0.000yuan/1,000 tokens           | 0.000yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-7b     | 0.0005yuan/1,000 tokens          | 0.001yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-14b    | 0.001yuan/1,000 tokens           | 0.003yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-32b    | 0.002yuan/1,000 tokens           | 0.006yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-llama-8b    | 0.000yuan/1,000 tokens           | 0.000yuan/1,000 tokens         |
-| DeepSeek-R1-Distill | deepseek-r1-distill-llama-70b   | 0.000yuan/1,000 tokens           | 0.000yuan/1,000 tokens         |
+| Model               | sub-type                      | Input Price per 1K characters | Output Price per 1K characters |
+| ------------------- | ----------------------------- | ----------------------------- | ------------------------------ |
+| Qwen-Max            | qwen-max                      | 0.0024yuan/1,000 tokens       | 0.0096yuan/1,000 tokens        |
+| Qwen-Qlus           | qwen-plus                     | 0.0008yuan/1,000 tokens       | 0.008yuan/1,000 tokens         |
+| Qwen-Turbo          | qwen-turbo                    | 0.0003yuan/1,000 tokens       | 0.003yuan/1,000 tokens         |
+| Qwq-Plus            | qwq-plus                      | 0.0016yuan/1,000 tokens       | 0.004yuan/1,000 tokens         |
+| Qwen-Long           | qwen-long                     | 0.0005yuan/1,000 tokens       | 0.002yuan/1,000 tokens         |
+| Qwen3               | qwen3-235b-a22b-thinking-2507 | 0.002yuan/1,000 tokens        | 0.02yuan/1,000 tokens          |
+| Qwen3               | qwen3-235b-a22b-instruct-2507 | 0.002yuan/1,000 tokens        | 0.008yuan/1,000 tokens         |
+| Qwen3               | qwen3-30b-a3b-thinking-2507   | 0.00075yuan/1,000 tokens      | 0.0075yuan/1,000 tokens        |
+| Qwen3               | qwen3-30b-a3b-instruct-2507   | 0.00075yuan/1,000 tokens      | 0.003yuan/1,000 tokens         |
+| Qwen3               | qwen3-32b                     | 0.002yuan/1,000 tokens        | 0.02yuan/1,000 tokens          |
+| Qwen3               | qwen3-30b-a3b                 | 0.00075yuan/1,000 tokens      | 0.0075yuan/1,000 tokens        |
+| Qwen3               | qwen3-14b                     | 0.001yuan/1,000 tokens        | 0.01yuan/1,000 tokens          |
+| Qwen3               | qwen3-8b                      | 0.0005yuan/1,000 tokens       | 0.005yuan/1,000 tokens         |
+| Qwen3               | qwen3-4b                      | 0.0003yuan/1,000 tokens       | 0.003yuan/1,000 tokens         |
+| Qwen3               | qwen3-1.7b                    | 0.0003yuan/1,000 tokens       | 0.003yuan/1,000 tokens         |
+| Qwen3               | qwen3-0.6b                    | 0.0003yuan/1,000 tokens       | 0.003yuan/1,000 tokens         |
+| QwQ                 | qwq-32b                       | 0.002yuan/1,000 tokens        | 0.006yuan/1,000 tokens         |
+| DeepSeek-V3.1       | deepseek-v3.1                 | 0.004yuan/1,000 tokens        | 0.012yuan/1,000 tokens         |
+| DeepSeek-R1         | deepseek-r1                   | 0.004yuan/1,000 tokens        | 0.016yuan/1,000 tokens         |
+| DeepSeek-V3         | deepseek-v3                   | 0.002yuan/1,000 tokens        | 0.008yuan/1,000 tokens         |
+| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-7b   | 0.0005yuan/1,000 tokens       | 0.001yuan/1,000 tokens         |
+| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-14b  | 0.001yuan/1,000 tokens        | 0.003yuan/1,000 tokens         |
+| DeepSeek-R1-Distill | deepseek-r1-distill-qwen-32b  | 0.002yuan/1,000 tokens        | 0.006yuan/1,000 tokens         |
+| Kimi-K2             | Moonshot-Kimi-K2-Instruct     | 0.004yuan/1,000 tokens        | 0.016yuan/1,000 tokens         |
 `
 }
 
 func (p *AlibabacloudModelProvider) calculatePrice(modelResult *ModelResult) error {
 	price := 0.0
 	priceTable := map[string][2]float64{
+		"qwen-max":                      {0.0024, 0.0096},
+		"qwen-plus":                     {0.0008, 0.008},
+		"qwen-turbo":                    {0.0003, 0.003},
+		"qwq-plus":                      {0.0016, 0.004},
 		"qwen-long":                     {0.0005, 0.002},
-		"qwen-turbo":                    {0.002, 0.006},
-		"qwen-plus":                     {0.004, 0.012},
-		"qwen-max":                      {0.040, 0.120},
-		"qwen-max-longcontext":          {0.040, 0.120},
-		"qwen3-235b-a22b":               {0.004, 0.04},
+		"qwen3-235b-a22b-thinking-2507": {0.002, 0.02},
+		"qwen3-235b-a22b-instruct-2507": {0.002, 0.008},
+		"qwen3-30b-a3b-thinking-2507":   {0.00075, 0.0075},
+		"qwen3-30b-a3b-instruct-2507":   {0.00075, 0.003},
 		"qwen3-32b":                     {0.002, 0.02},
-		"deepseek-r1":                   {0.002, 0.008},
-		"deepseek-v3":                   {0.001, 0.004},
-		"deepseek-r1-distill-qwen-1.5b": {0.000, 0.000},
-		"deepseek-r1-distill-qwen-7b":   {0.001, 0.003},
-		"deepseek-r1-distill-qwen-14b ": {0.002, 0.006},
-		"deepseek-r1-distill-qwen-32b":  {0.000, 0.000},
-		"deepseek-r1-distill-llama-8b":  {0.000, 0.000},
+		"qwen3-30b-a3b":                 {0.00075, 0.0075},
+		"qwen3-14b":                     {0.001, 0.01},
+		"qwen3-8b":                      {0.0005, 0.005},
+		"qwen3-4b":                      {0.0003, 0.003},
+		"qwen3-1.7b":                    {0.0003, 0.003},
+		"qwen3-0.6b":                    {0.0003, 0.003},
+		"qwq-32b":                       {0.002, 0.006},
+		"deepseek-v3.1":                 {0.004, 0.012},
+		"deepseek-r1":                   {0.004, 0.016},
+		"deepseek-v3":                   {0.002, 0.008},
+		"deepseek-r1-distill-qwen-7b":   {0.0005, 0.001},
+		"deepseek-r1-distill-qwen-14b":  {0.001, 0.003},
+		"deepseek-r1-distill-qwen-32b":  {0.002, 0.006},
+		"Moonshot-Kimi-K2-Instruct":     {0.004, 0.016},
 	}
 
 	if priceItem, ok := priceTable[p.subType]; ok {
