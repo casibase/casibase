937bdf6bf80100acf007c70f73735f98d25e8d43
ShixingZhou <53111724+FlySky-z@users.noreply.github.com>
2025-09-06 20:05:26 +0800

    feat: update the frontend i18n (#1572)

--- web/src/TemplateEditPage.js
+++ web/src/TemplateEditPage.js
@@ -130,7 +130,7 @@ class TemplateEditPage extends React.Component {
         </Row>
         <Row style={{marginTop: "20px"}} >
           <Col style={{marginTop: "5px"}} span={(Setting.isMobile()) ? 22 : 2}>
-            {Setting.getLabel(i18next.t("general:Readme"), i18next.t("general:Readme - Tooltip"))} :
+            {Setting.getLabel(i18next.t("template:Readme"), i18next.t("template:Readme - Tooltip"))} :
           </Col>
           <Col span={22} >
             <Input value={this.state.template.readme} onChange={e => {
