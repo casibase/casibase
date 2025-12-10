3b5a93dca55dc6cd4725bd898d2748c4b3d880fd
Yang Luo <hsluoyz@gmail.com>
2025-12-10 15:44:20 +0800

    Update permission.go

--- util/permission.go
+++ util/permission.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Casibase Authors. All Rights Reserved.
+// Copyright 2025 The Casibase Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,9 +14,7 @@
 
 package util
 
-import (
-	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
-)
+import "github.com/casdoor/casdoor-go-sdk/casdoorsdk"
 
 const (
 	UserTypeChatAdmin       = "chat-admin"
